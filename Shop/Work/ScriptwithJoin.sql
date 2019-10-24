df72f74ec6ee09144d3165936aee702a3bb162a9
Danil <danil.kozlov111@gmail.com>
2019-10-24 20:26:00 +0300

    24.10.2019 joins complete

--- Shop/Work/ScriptwithJoin.sql
+++ Shop/Work/ScriptwithJoin.sql
@@ -20,11 +20,60 @@ ON prov.Id = purch.ProvidersId
 
 WHERE purch.ProvidersId = 1;
 
-SELECT * FROM Products AS p
-JOIN 
+SELECT CONCAT( N'Количество товара за промежуток времени: ' ,SUM(pr.Quantity) )AS Result
+FROM Purchases AS pr 
+
+WHERE Datatime BETWEEN '26.09.2019' AND '30.09.2019'
+
+
+SELECT SUM(Quantity) as N'Количество товара на текущий момент времени' FROM (
+SELECT Quantity FROM Purchases 
+JOIN Products
+ON Products.Id = Purchases.ProductId
+WHERE Products.Id = 2
+UNION
+SELECT (Quantity * -1) FROM Sales JOIN Products ON Products.Id = Sales.ProductId
+WHERE Products.Id = 2
+)AS T;
+
+
+SELECT SUM(Quantity) as N'Количество товара на текущий момент времени' FROM 
+(
+
+SELECT Quantity FROM Purchases 
+JOIN Products
+ON Products.Id = Purchases.ProductId
+WHERE Products.Id = 2
+
+UNION
+
+SELECT (Quantity * -1) FROM Sales
+JOIN Products
+ON Products.Id = Sales.ProductId
+WHERE Products.Id = 2
+
+)AS T;
+
+Select Sum(Quantity) as 'QTY'
+from
+(
+
+select Quantity from Purchases as purch
+join 
 ProductCategories pc
-ON p.Id = pc.ProductId
-JOIN
-Categories c
-ON c.Id = pc.CategoryId
-WHERE c.Id = 4;
+on pc.ProductId = purch.ProductId
+where pc.CategoryId = 1 AND Datatime <='25.09.2019'
+
+union all
+
+select (Quantity * -1) from Sales as s
+join 
+ProductCategories pc
+on pc.ProductId = s.ProductId
+where pc.CategoryId = 1 AND Datatime <='25.09.2019'
+
+)as T;
+
+
+
+
